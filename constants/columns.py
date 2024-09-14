4e4a2d51701110ecb74c02c5ba11788e9e63fd5c
Richard Song <richardsong@richards-mbp.lan>
2024-09-13 22:51:00 -0500

    Update bbref scraper
    - add args to pass in to specify workflow for scraper
    to grab and parse data
    - add capability to fetch data from bbref directly
    or from html files saved if they are available
    - fetching from existing files can avoid spamming requests
    to bbref with web scraping

--- constants/columns.py
+++ constants/columns.py
@@ -1,234 +1,269 @@
-METRICS_STATS_HEADERS = {
+METRICS_CATEGORIES = {
     'totals': {
+        'identifier': 'totals',
+        'name': 'totals'
+    },
+    'per_g': {
+        'identifier': 'per_game',
+        'name': 'per_game'
+    },
+    'per_m': {
+        'identifier': 'per_minute',
+        'name': 'per_minute'
+    },
+    'per_p': {
+        'identifier': 'per_poss',
+        'name': 'per_poss'
+    },
+    'adv': {
+        'identifier': 'advanced',
+        'name': 'advanced'
+    },
+    'pbp': {
+        'identifier': 'pbp',
+        'name': 'play-by-play'
+    },
+    'shooting': {
+        'identifier': 'shooting',
+        'name': 'shooting'
+    },
+    'adv_shooting': {
+        'identifier': 'adv_shooting',
+        'name': 'adv_shooting'
+    }
+}
+
+METRICS_STATS_HEADERS = {
+    METRICS_CATEGORIES['totals']['name']: {
         'name': 'Totals',
         'description': 'Season Totals',
-        'stats': [
-            {
+        'stats': {
+            'fg': {
                 'name': 'Field Goals',
                 'abbreviation': 'FG',
                 'description': 'Field Goals made'
             },
-            {
+            'fga': {
                 'name': 'Field Goal Attempts',
                 'abbreviation': 'FGA',
                 'description': 'Field Goals attempted'
             },
-            {
+            'fg_pct': {
                 'name': 'Field Goal Percentage',
                 'abbreviation': 'FG%',
                 'description': 'Percent of Field Goals made'
             },
-            {
+            'fg3': {
                 'name': '3-Point Field Goals',
                 'abbreviation': '3P',
                 'description': '3-Point Field Goals made'
             },
-            {
+            'fg3a': {
                 'name': '3-Point Field Goal Attempts',
                 'abbreviation': '3PA',
                 'description': '3-Point Field Goals attempted'
             },
-            {
+            'fg3_pct': {
                 'name': '3-Point Field Goal Percentage',
                 'abbreviation': '3P%',
                 'description': 'Percent of 3-Point Field Goals made'
             },
-            {
+            'fg2': {
                 'name': '2-Point Field Goals',
                 'abbreviation': '2P',
                 'description': '2-Point Field Goals made'
             },
-            {
+            'fg2a': {
                 'name': '2-Point Field Goal Attempts',
                 'abbreviation': '2PA',
                 'description': '2-Point Field Goals attempted'
             },
-            {
+            'fg2_pct': {
                 'name': '2-Point Field Goal Percentage',
                 'abbreviation': '2P%',
                 'description': 'Percent of 2-Point Field Goals made'
             },
-            {
+            'efg_pct': {
                 'name': 'Effective Field Goal Percentage',
                 'abbreviation': 'eFG%',
                 'description': 'Weighted combined Field Goal Percentage between 3 and 2-Pointers'
             },
-            {
+            'ft': {
                 'name': 'Free Throws',
                 'abbreviation': 'FT',
                 'description': 'Free Throws made'
             },
-            {
+            'fta': {
                 'name': 'Free Throw Attempts',
                 'abbreviation': 'FTA',
                 'description': 'Free Throws attempted'
             },
-            {
+            'ft_pct': {
                 'name': 'Free Throw Percentage',
                 'abbreviation': 'FT%',
                 'description': 'Percent of Free Throws made'
             },
-            {
+            'orb': {
                 'name': 'Offensive Rebounds',
                 'abbreviation': 'ORB',
                 'description': 'Rebounds while on offense'
             },
-            {
+            'drb': {
                 'name': 'Defensive Rebounds',
                 'abbreviation': 'DRB',
                 'description': 'Rebounds while on defense'
             },
-            {
+            'trb': {
                 'name': 'Total Rebounds',
                 'abbreviation': 'TRB',
                 'description': 'Total Rebounds made'
             },
-            {
+            'ast': {
                 'name': 'Assists',
                 'abbreviation': 'AST',
                 'description': 'Passes made that led to a made basket'
             },
-            {
+            'stl': {
                 'name': 'Steals',
                 'abbreviation': 'STL',
                 'description': 'Balls taken from offensive team'
             },
-            {
+            'blk': {
                 'name': 'Blocks',
                 'abbreviation': 'BLK',
                 'description': 'Shots blocked while on defense'
             },
-            {
+            'tov': {
                 'name': 'Turnovers',
                 'abbreviation': 'TOV',
                 'description': 'Balls lost to defensive team'
             },
-            {
+            'pf': {
                 'name': 'Personal Fouls',
                 'abbreviation': 'PF',
                 'description': 'Fouls committed'
             },
-            {
+            'pts': {
                 'name': 'Points',
                 'abbreviation': 'PTS',
                 'description': 'buckets.'
             }
-        ]
+        }
     },
-    'pergame': {
+    METRICS_CATEGORIES['per_g']['name']: {
         'name': 'Per Game',
         'description': 'Per Game Averages',
-        'stats': [
-            {
+        'stats': {
+            'fg_per_g': {
                 'name': 'Field Goals',
                 'abbreviation': 'FG',
                 'description': 'Field Goals made'
             },
-            {
+            'fga_per_g': {
                 'name': 'Field Goal Attempts',
                 'abbreviation': 'FGA',
                 'description': 'Field Goals attempted'
             },
-            {
+            'fg_pct': {
                 'name': 'Field Goal Percentage',
                 'abbreviation': 'FG%',
                 'description': 'Percent of Field Goals made'
             },
-            {
+            'fg3_per_g': {
                 'name': '3-Point Field Goals',
                 'abbreviation': '3P',
                 'description': '3-Point Field Goals made'
             },
-            {
+            'fg3a_per_g': {
                 'name': '3-Point Field Goal Attempts',
                 'abbreviation': '3PA',
                 'description': '3-Point Field Goals attempted'
             },
-            {
+            'fg3_pct': {
                 'name': '3-Point Field Goal Percentage',
                 'abbreviation': '3P%',
                 'description': 'Percent of 3-Point Field Goals made'
             },
-            {
+            'fg2_per_g': {
                 'name': '2-Point Field Goals',
                 'abbreviation': '2P',
                 'description': '2-Point Field Goals made'
             },
-            {
+            'fg2a_per_g': {
                 'name': '2-Point Field Goal Attempts',
                 'abbreviation': '2PA',
                 'description': '2-Point Field Goals attempted'
             },
-            {
+            'fg2_pct': {
                 'name': '2-Point Field Goal Percentage',
                 'abbreviation': '2P%',
                 'description': 'Percent of 2-Point Field Goals made'
             },
-            {
+            'efg_pct': {
                 'name': 'Effective Field Goal Percentage',
                 'abbreviation': 'eFG%',
                 'description': 'Weighted combined Field Goal Percentage between 3 and 2-Pointers'
             },
-            {
+            'ft_per_g': {
                 'name': 'Free Throws',
                 'abbreviation': 'FT',
                 'description': 'Free Throws made'
             },
-            {
+            'fta_per_g': {
                 'name': 'Free Throw Attempts',
                 'abbreviation': 'FTA',
                 'description': 'Free Throws attempted'
             },
-            {
+            'ft_pct': {
                 'name': 'Free Throw Percentage',
                 'abbreviation': 'FT%',
                 'description': 'Percent of Free Throws made'
             },
-            {
+            'orb_per_g': {
                 'name': 'Offensive Rebounds',
                 'abbreviation': 'ORB',
                 'description': 'Rebounds while on offense'
             },
-            {
+            'drb_per_g': {
                 'name': 'Defensive Rebounds',
                 'abbreviation': 'DRB',
                 'description': 'Rebounds while on defense'
             },
-            {
+            'trb_per_g': {
                 'name': 'Total Rebounds',
                 'abbreviation': 'TRB',
                 'description': 'Total Rebounds made'
             },
-            {
+            'ast_per_g': {
                 'name': 'Assists',
                 'abbreviation': 'AST',
                 'description': 'Passes made that led to a made basket'
             },
-            {
+            'stl_per_g': {
                 'name': 'Steals',
                 'abbreviation': 'STL',
                 'description': 'Balls taken from offensive team'
             },
-            {
+            'blk_per_g': {
                 'name': 'Blocks',
                 'abbreviation': 'BLK',
                 'description': 'Shots blocked while on defense'
             },
-            {
+            'tov_per_g': {
                 'name': 'Turnovers',
                 'abbreviation': 'TOV',
                 'description': 'Balls lost to defensive team'
             },
-            {
+            'pf_per_g': {
                 'name': 'Personal Fouls',
                 'abbreviation': 'PF',
                 'description': 'Fouls committed'
             },
-            {
+            'pts_per_g': {
                 'name': 'Points',
                 'abbreviation': 'PTS',
                 'description': 'buckets.'
             }
-        ]
+        }
     }
 }
--- scrapers/BbrefScraper.py
+++ scrapers/BbrefScraper.py
@@ -6,42 +6,84 @@ import ssl
 import urllib.request as request
 from urllib.parse import parse_qs, urlparse
 from bs4 import BeautifulSoup as bs
+from constants.columns import METRICS_CATEGORIES, METRICS_STATS_HEADERS
 
 class BbrefScraper:
-    def __init__(self, metric):
+    def __init__(self, **kwargs):
         self.base_url = 'https://www.basketball-reference.com/'
         self.ssl = ssl.create_default_context(cafile=certifi.where())
-        self.metric = metric
+        self.metric_key = kwargs['metric']
+        self.start = kwargs['start']
+        self.end = kwargs['end']
+        self.debug = kwargs['debug']
 
-    def scrape_player_metrics_per_season_html(self, start: int, end: int):
-        stat_metrics = {
-            'totals': 'totals',
-            'pergame': 'per_game',
-            'per36': 'per_minute',
-            'per100': 'per_poss',
-            'advanced': 'advanced',
-            'pbp': 'play-by-play',
-            'shooting': 'shooting',
-            'adjshooting': 'adj_shooting'
-        }
+    def scrape_player_metrics_from_url(self):
+        stat_metric = METRICS_CATEGORIES[self.metric_key]['name']
 
-        htmls_dir = f'players_{self.metric}_per_season_html'
-        if not os.path.exists(htmls_dir):
-            os.mkdir(htmls_dir)
-
-        stat_metric = stat_metrics[self.metric]
-
-        urls = [{'year': year, 'url': f'{self.base_url}leagues/NBA_{year}_{stat_metric}.html'} for year in range(start, end+1)]
+        urls = [{'year': year, 'url': f'{self.base_url}leagues/NBA_{year}_{stat_metric}.html'} for year in range(self.start, self.end+1)]
         for url in urls:
             print(f'fetching from: {url}')
             per_player_stats_html = request.urlopen(url['url'], context=self.ssl).read().decode('utf-8')
             year = url['year']
-            with open(f'{htmls_dir}/players_{self.metric}_{year}.html', 'w') as stats_html:
-                stats_html.write(per_player_stats_html)
 
-    def parse_player_data(self):
-        return
+            if self.debug:
+                htmls_dir = f'files/html/{year}'
+                if not os.path.exists(htmls_dir):
+                    os.makedirs(htmls_dir)
+                with open(f'{htmls_dir}/players_{self.metric_key}_{year}.html', 'w') as stats_html:
+                    print(f'saving {year} {self.metric_key} html')
+                    stats_html.write(per_player_stats_html)
+            
+            self.__parse_player_data_html(per_player_stats_html, year)
+
+    def scrape_player_metrics_from_html(self):
+        files = [{'file': f'files/html/{year}/players_{self.metric_key}_{year}.html', 'year': year} for year in range(self.start, self.end+1)]
+        for file in files:
+            f = file['file']
+            y = file['year']
+            try:
+                with open(f, 'r') as html_file:
+                    print(f'parsing {f} for player data')
+                    self.__parse_player_data_html(html_file.read(), y)
+            except FileNotFoundError:
+                print(f'{f} was not found. skipping')
+    
+    def __parse_player_data_html(self, html, year: int):
+        html_soup = bs(html, 'html.parser')
+        metric_identifier = METRICS_CATEGORIES[self.metric_key]['identifier']
+        table = html_soup.find(id=f'{metric_identifier}_stats')
+        tbody = table.find('tbody')
+        rows = tbody.find_all('tr', class_=lambda class_: class_ is None)
+        player_stats = []
+        for p_row in rows:
+            player_name = p_row.find_all('td', attrs={'data-stat': 'name_display'})
+            position = p_row.find_all('td', attrs={'data-stat': 'pos'})
+            stats = p_row.find_all('td', attrs={'data-stat': lambda stat: stat in METRICS_STATS_HEADERS[self.metric_key]['stats']})
+            player_stats.append({
+                'name': player_name[0].text,
+                'position': position[0].text,
+                'stats': {stat.get('data-stat'): float(stat.text) if stat.text else '' for stat in stats}
+            })
+        if self.debug:
+            stats_dir = f'files/stats/{year}'
+            if not os.path.exists(stats_dir):
+                os.makedirs(stats_dir)
+            with open(f'{stats_dir}/{self.metric_key}.json', 'w') as stats_file:
+                print(f'saving {year} {self.metric_key} stats json')
+                json.dump(player_stats, stats_file)
 
 if __name__ == '__main__':
-    scraper = BbrefScraper('pergame')
-    scraper.scrape_player_metrics_per_season_html(2020, 2024)
+    parser = argparse.ArgumentParser(description='args for player stat parsing')
+    parser.add_argument('-m', '--metric', type=str, choices=METRICS_CATEGORIES.keys(), required=True, help='metric to grab stats for')
+    parser.add_argument('-s', '--start', type=int, required=True, help="start year to get stats for")
+    parser.add_argument('-e', '--end', type=int, required=True, help="end year to get stats for")
+    parser.add_argument('--debug', action='store_true', help='save html files and player stats. skips saving to db')
+    parser.add_argument('--source', type=str, choices=['url', 'html'], default='html', help='get player data from url (bball-ref) or saved html files (from debug)')
+
+    print(parser.parse_args())
+    kwargs = vars(parser.parse_args())
+    scraper = BbrefScraper(**kwargs)
+    if kwargs['source'] == 'url':
+        scraper.scrape_player_metrics_from_url()
+    else:
+        scraper.scrape_player_metrics_from_html()
